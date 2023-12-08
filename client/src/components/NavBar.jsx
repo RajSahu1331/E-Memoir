55f2b317547dd7420f0b2512fd8d36399515cf7e
RajSahu1331 <rajsahu1331@gmail.com>
2023-12-08 18:44:28 +0530

    Initial Commit

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,41 @@
+import { Link } from 'react-router-dom';
+import { useAuthContext } from '../hooks/useAuthContext';
+import { useLogout } from '../hooks/useLogout.js';
+import styles from '../styles/styles.module.scss'
+
+const NavBar = () => {
+    const { user } = useAuthContext();
+    const { logout } = useLogout();
+    const handleClick = () => logout();
+
+    return (
+        <header>
+            <nav className={styles.header}>
+                <ul>
+                    <li>
+                        <Link to="/"> Home </Link>
+                    </li>
+                </ul>
+            </nav>
+
+            <span> The Diary App </span>
+
+            {user ? (
+                <div>
+                    <span>{user.email}</span>
+                    <button onClick={handleClick}>
+                        Logout
+                    </button>
+                </div>
+
+            ) : (
+                <div>
+                    <Link to="/api/login"> Login </Link>
+                    <Link to="/api/signup"> Signup </Link>
+                </div>
+            )}
+        </header>
+    );
+};
+
+export default NavBar;
