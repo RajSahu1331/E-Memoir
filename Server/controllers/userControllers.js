1a7f2311fea1955ba4ee71260adf23ceca955429
RajSahu1331 <rajsahu1331@gmail.com>
2024-02-20 18:58:26 +0530

    update

--- Server/controllers/userControllers.js
+++ Server/controllers/userControllers.js
@@ -1,25 +1,12 @@
-<<<<<<< HEAD
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
 const createToken = (_id) => {
   return jwt.sign({ _id }, process.env.JWT_SECRET, { expiresIn: "30d" });
-=======
-import jwt from 'jsonwebtoken';
-import User from '../models/User.js';
-
-const createToken = _id => {
-    return jwt.sign(
-        { _id },
-        process.env.JWT_SECRET,  
-        { expiresIn: '3d' }
-    );
->>>>>>> 6f4f9cbf364843a30155649354dc71b07d8c7e16
 };
 
 // signup
 export const signupUser = async (req, res) => {
-<<<<<<< HEAD
   const { email, password } = req.body;
 
   try {
@@ -29,22 +16,10 @@ export const signupUser = async (req, res) => {
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
-=======
-    const { email, password } = req.body;
-
-    try {
-        const user = await User.signup(email, password);
-        const token = createToken(user._id);
-        res.status(200).json({ email, token });
-    } catch (err) {
-        res.status(400).json({ error: err.message });
-    }
->>>>>>> 6f4f9cbf364843a30155649354dc71b07d8c7e16
 };
 
 // login
 export const loginUser = async (req, res) => {
-<<<<<<< HEAD
   const { email, password } = req.body;
 
   try {
@@ -55,15 +30,3 @@ export const loginUser = async (req, res) => {
     res.status(400).json({ error: err.message });
   }
 };
-=======
-    const { email, password } = req.body;
-
-    try {
-        const user = await User.login(email, password);
-        const token = createToken(user._id);
-        res.status(200).json({ email, token });
-    } catch (err) {
-        res.status(400).json({ error: err.message });
-    }
-};
->>>>>>> 6f4f9cbf364843a30155649354dc71b07d8c7e16
